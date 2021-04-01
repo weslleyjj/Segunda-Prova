8bc1aa52029d5140d86750b0729610971dc97c6e
Jodeilson Weslley <jodeilson@ufrn.edu.br>
2021-04-01 18:57:01 -0300

    Definido os atributos principais no data class

---
+++ app/src/main/java/com/pdm/segundaprova/data/Veiculo.kt
@@ -0,0 +1,5 @@
+package com.pdm.segundaprova.data
+
+data class Veiculo(val id: Int, val modelo: String, val cor: String, val ano: Int, val preco: Float, val estoque: Int, val prontoParaRetirada: Boolean) {
+
+}
